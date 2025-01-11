f6028bd5eb6715a4c8b7f8d2999f8daf296dfb78
marcusczh <marcuschuazh@gmail.com>
2025-01-11 14:35:30 +0800

    Remove comment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,13 +39,13 @@ function App() {
     const path = window.location.pathname;
 
     // If user is not authenticated, show login/register form
-    /*if (!isUserAuthenticated) {
+    if (!isUserAuthenticated) {
       return (
         <LoginRegisterForm
           setUserAuthenticatedStatus={setUserAuthenticatedStatus}
         />
       );
-    }*/
+    }
 
     // Render pages based on pathname
     switch (path) {
