8d738656c8de739f9574ca5f58bbeca0a1525554
JinxinZhao315 <34888060+JinxinZhao315@users.noreply.github.com>
2025-01-11 10:48:12 +0800

    Setup DB

--- server/index.js
+++ server/index.js
@@ -4,33 +4,34 @@ import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
-const databaseURL = process.env.DATABASE_URL;
+const databaseURL = "mongodb+srv://dbs_hack:AbC123456@cluster0.pfsb0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+try {
+  await mongoose.connect(databaseURL) // This returns a promise
+  console.log('MongoDB Connected')
+} catch (err) {
+  console.error('MongoDB Connection Failed. ', err.message)
+  process.exit(1);
+}
 
 app.use(cors({
-    origin: [process.env.ORIGIN],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true, // we need to enable cookies hence credentials need to be set to true
+  origin: [process.env.ORIGIN],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true, // we need to enable cookies hence credentials need to be set to true
 }));
 
 app.use(cookieParser());
 app.use(express.json());
 
 
+app.get('/', (req, res) => res.send("API Running"))
+
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-setupSocket(server);
-
-mongoose.connect(databaseURL)
-  .then(() => {
-    console.log("Connected to the database");
-  })
-  .catch((error) => {
-    console.error("Database connection failed:", error.message);
-  });
+// setupSocket(server);
